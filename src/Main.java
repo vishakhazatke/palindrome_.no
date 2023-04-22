0169268ea9634ea74af4ca70f8e1506bf41aa1e8
vishakhazatke <vzatke21@gmail.com>
2023-04-22 18:12:32 +0530

    Reverse_no.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
-class Palindrome{
-    public static void main(String[] args){
-        int num = 2332;
+class Reverse {
+    public static void main(String[] args) {
+        int num = 13124456;
         int rev = 0;
         int temp = num;
-        while(temp !=0) {
-            rev = rev * 10 + (temp % 10);
-            temp = temp / 10;
+        while(temp!=0){
+            rev = rev*10 + (temp%10);
+            temp = temp/10;
         }
-        if (num == rev){
-            System.out.println("it is a palindrome no");
-        }
-        else{
-            System.out.println("it is not palindrome no");
-        }
+        System.out.println("The reverse no is " + rev);
+
     }
 }
